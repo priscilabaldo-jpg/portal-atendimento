81ba02b28848a832c69f4a0a1691e506b9815fc9
Matheus Alves Mendes <matheus.mendes@leveros.com.br>
2026-08-31 14:51:05 -0300

    Update conv.js

--- conv.js
+++ conv.js
@@ -2,7 +2,7 @@
 // Arquivo de configurações isolado
 
 const APP_CONFIG = {
-    // Cole sua chave real do Google AI Studio abaixo, mantendo as aspas
+    // Cole sua chave real do Google AI Studio abaixo, mantendo as aspas]
     GEMINI_API_KEY: "AQ.Ab8RN6KCt9x928NCwXiMAesEQ0sGdZFVqolbJ_bbh-9JTAwpPQ",
     
     // Você também pode definir o modelo padrão aqui
